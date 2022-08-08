04e8ebd767a44d7aa0ff39ba878d4dfb8a37d107
abrish-software <97157856+abrish-software@users.noreply.github.com>
2022-08-08 18:01:34 +0300

    Update print.c

--- print.c
+++ print.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * check_buffer_overflow - if writing over buffer space,
@@ -99,26 +99,4 @@ int _printf(const char *format, ...)
 	}
 	write_buffer(buffer, len, list);
 	return (total_len);
-}
-
-/**
- * main - sample main program
- * Return: 0 on sucess
- */
-int main(void)
-{
-	_printf("\n\n\nHere's some examples of what you could do with this custom_printf function!\n\n\n");
-	sleep(1);
-	_printf("\nPrinting Strings, Characters, and Numbers...... %s %c%drld\n\n", "Hello", 'W', 0);
-	sleep(1);
-	_printf("Printing Reverse...... %r \n\n", "Hello");
-	sleep(1);
-	_printf("Printing Binary (base 2)...... %b \n\n", "Hello");
-	sleep(1);
-	_printf("Printing Octal (base 8)...... %o \n\n", "Hello");
-	sleep(1);
-	_printf("Printing Rot13 (encrypt)...... %R \n\n", "Hello");
-	sleep(1);
-	_printf("\n\n             = )                  \n\n\n");
-	return (0);
 }

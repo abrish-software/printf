cee13a45962daa42f8f8c13b2b2e132506da01ba
meharyalx <107629458+meharyalx@users.noreply.github.com>
2022-08-09 02:04:22 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -4,11 +4,11 @@
  * @format: identifier to look for.
  * Return: the length of the string.
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
 	convert_match m[] = {
 		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37}, {"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%%", printf_37}, {"%i", printf_int}, {"%r", printf_srev},
 		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
 		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
 		{"%S", printf_exclusive_string}, {"%p", printf_pointer}

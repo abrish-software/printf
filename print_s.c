548c2c73ffd81424dbf51eda203b52b45fbc58ca
meharyalx <107629458+meharyalx@users.noreply.github.com>
2022-08-08 17:10:12 +0300

    Update print_s.c

--- print_s.c
+++ print_s.c
@@ -8,12 +8,19 @@
  */
 int print_s(va_list s)
 {
-	char *str = va_arg(s, char *);
-	int i = 0;
+	char *s;
+	char *p;
+	int len;
 
-	if (str == NULL)
-		str = "(null)";
-	while (str[i])
-		_putchar(str[i++]);
-	return (i);
+	s = va_arg(list, char *);
+	if (s == NULL)
+		s = "(null)";
+
+	len = _strlen(s);
+
+	p = malloc(sizeof(char) * len + 1);
+	if (p == NULL)
+		return (NULL);
+
+	return (_strcpy(p, s));
 }

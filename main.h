ebb0ba0c407e389e6d411943b0e6d42191653ed4
abrish-software <97157856+abrish-software@users.noreply.github.com>
2022-08-09 04:33:26 +0300

    Update main.h

--- main.h
+++ main.h
@@ -35,4 +35,5 @@ int printf_hex(va_list args);
 int printf_oct(va_list args);
 int printf_unsigned(va_list args);
 int printf_exclusive_string(va_list args);
+int printf_HEX_aux(unsigned int num);
 #endif

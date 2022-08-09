d79c3eb2c6220f0f05ada2020db96fc4326ae1d4
meharyalx <107629458+meharyalx@users.noreply.github.com>
2022-08-09 09:50:05 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <unistd.h>
+
 /**
  * _printf - Emulate the original.
  *
  * @format: Format by specifier.
- *
  * Return: count of chars.
  */
 int _printf(const char *format, ...)

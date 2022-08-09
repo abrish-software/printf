a0d6d63033fa33d9fd735e983276a445fb428ddc
abrish-software <97157856+abrish-software@users.noreply.github.com>
2022-08-09 04:12:36 +0300

    Update printf_exclusive_string.c

--- printf_exclusive_string.c
+++ printf_exclusive_string.c
@@ -5,35 +5,32 @@
  * Return: the length of the string.
  */
 
-int printf_exclusive_string(va_list args)
+int printf_exclusive_string(unsigned int num)
 {
-	char *s;
-	int i, len = 0;
-	int cast;
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int temp = num;
 
-	s = va_arg(args, char *);
-	if (s == NULL)
-		s = "(null)";
-	for (i = 0; s[i] != '\0'; i++)
+	while (num / 16 != 0)
 	{
-		if (s[i] < 32 || s[i] >= 127)
-		{
-			_putchar('\\');
-			_putchar('x');
-			len = len + 2;
-			cast = s[i];
-			if (cast < 16)
-			{
-				_putchar('0');
-				len++;
-			}
-			len = len + printf_HEX_aux(cast);
-		}
-		else
-		{
-			_putchar(s[i]);
-			len++;
-		}
+		num /= 16;
+		counter++;
 	}
-	return (len);
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 7;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
 }

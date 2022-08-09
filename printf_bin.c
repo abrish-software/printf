7ab11d7b818836307f801d13fc04deab20dc5818
abrish-software <97157856+abrish-software@users.noreply.github.com>
2022-08-09 03:34:17 +0300

    Update printf_bin.c

--- printf_bin.c
+++ printf_bin.c
@@ -6,12 +6,12 @@
  * @val: arguments.
  * Return: 1.
  */
-int printf_bin(va_list val)
+int printf_bin(va_list args)
 {
 	int flag = 0;
 	int cont = 0;
 	int i, a = 1, b;
-	unsigned int num = va_arg(val, unsigned int);
+	unsigned int num = va_arg(args, unsigned int);
 	unsigned int p;
 
 	for (i = 0; i < 32; i++)
